2f61b5d7c5f40b39ab1de0931173552b257e2cfa
Xtr126 <k.gunetilleke@gmail.com>
2025-08-01 19:18:43 +0530

    fix: workaround for NDK bug

--- app/src/main/java/com/xtr/compound/MainActivity.kt
+++ app/src/main/java/com/xtr/compound/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.xtr.compound
 
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
 import android.view.SurfaceHolder
+import android.view.SurfaceView
 import androidx.activity.ComponentActivity
 
 
@@ -25,7 +27,22 @@ class MainActivity : ComponentActivity(), SurfaceHolder.Callback2 {
             ?.let {
                 mCallback = ITinywlCallback.Stub.asInterface(it)
                 mCallback?.asBinder()?.linkToDeath(deathRecipient, 0).also {
-                    window.takeSurface(this)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                        /*
+                         * Only usable in Android 15 due to
+                         * ASurfaceControl_createFromWindow returning NULL
+                         * for root ANativeWindow: https://issuetracker.google.com/issues/320706287
+                         */
+                        window.takeSurface(this)
+                    } else {
+                        /*
+                         * On Android 14 and older we use a SurfaceView
+                         * instead of taking ownership of our window's surface.
+                         */
+                        val surfaceView = SurfaceView(this)
+                        setContentView(surfaceView)
+                        surfaceView.holder.addCallback(this)
+                    }
                 }
             }
     }
